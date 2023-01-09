afa1a256cf5e787520ee565884caad37388c021a
Edgar O <eandrei.olvera@gmail.com>
2023-01-08 21:48:21 -0600

    Se añade configuracion de bodyparser

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express')
+const express = require('express');
+const bodyParse = require('body-parser')
 const app = express()
 const api = require('./routes/routes');
+const port = process.env.PORT || 3001;
 
 //Servir contenido estatico
 
-app.use( express.json() )
+app.use(bodyParse.urlencoded({extended: true}));
+app.use(bodyParse.json())
 app.use('/api', api)
 
-app.get('/hola-mundo', (req, res)=>{
-    res.send('Hola mundo');
+app.get('/', (req, res)=>{
+    res.statusCode = 200;
+    res.json({ success:true, data:"Endpoint vivo" });
 })
 
 app.get('*',(req, res)=>{
@@ -17,4 +21,6 @@ app.get('*',(req, res)=>{
     res.status(404);
 })
 
-app.listen(80)
+app.listen(port, ()=>{
+    console.log(`Escuchando el puerto ${port}`)
+})
